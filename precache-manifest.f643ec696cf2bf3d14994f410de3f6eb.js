9896a794f603a3b087a3ae1e0581804529ed6cf6
Serhii Minin <minin.serhii@gmail.com>
2019-09-29 12:13:02 +0300

    Updates

---
+++ precache-manifest.f643ec696cf2bf3d14994f410de3f6eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45362d4bb07cc6bce331897caf764f4a",
+    "url": "/exchangerateapp/index.html"
+  },
+  {
+    "revision": "9b9567486fb0b6d9a621",
+    "url": "/exchangerateapp/static/js/2.a0b2369c.chunk.js"
+  },
+  {
+    "revision": "7396cedb14830bd1dbe3",
+    "url": "/exchangerateapp/static/js/main.0df88b7f.chunk.js"
+  },
+  {
+    "revision": "55c552d49f1aac2be1bc",
+    "url": "/exchangerateapp/static/js/runtime-main.19105e36.js"
+  }
+]);
